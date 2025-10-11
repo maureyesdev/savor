ee481a0c8cec640da5311315915b902b0277c62d
maureyesdev <maureyes.dev@gmail.com>
2025-10-11 14:23:14 -0600

    feat: add styling base to the project along with font and normalize library

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,6 @@
 import template from './header.html?raw';
-import { renderTemplate } from '../../core/render-template';
+import { renderTemplate } from '@/core/render-template';
+import './header.css';
 
 export function header({ element }) {
   element.innerHTML = renderTemplate(template);
